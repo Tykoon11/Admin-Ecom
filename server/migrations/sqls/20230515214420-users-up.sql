be2ea61fb88ecc6192ea2a2a876409d3b25e122d
Tykoon11 <noblemanunachukwu@gmail.com>
2023-06-02 23:49:58 +0100

    bycrypt hash update

--- server/migrations/sqls/20230515214420-users-up.sql
+++ server/migrations/sqls/20230515214420-users-up.sql
@@ -1,3 +1,3 @@
 /* Replace with your SQL commands */
 CREATE TABLE users (id SERIAL PRIMARY KEY, firstname VARCHAR(50), lastname VARCHAR(50), username VARCHAR(50), UNIQUE(username), password VARCHAR(255), role text);
-INSERT INTO users ( firstname, lastname, username, password, role) VALUES ('Nobleman', 'Unachukwu', 'Tykoon', 'password', 'admin');
+INSERT INTO users ( firstname, lastname, username, password, role) VALUES ('Nobleman', 'Unachukwu', 'Tykoon', '$2b$10$LFTbd.if1SoPhbx7x43X3.8AyhGmFQn2N587ZwKpqJ0et0ULmXBJ6', 'admin');
